01fdd74ba5f1a6b4f720010f72bb07815eb445ac
Henry Schmid <henry.schmid1@gmail.com>
2021-08-11 10:46:00 -0600

    Add close auction and date tests

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,6 +1,7 @@
 require './lib/item'
 require './lib/auction'
 require './lib/attendee'
+require 'date'
 
 RSpec.describe Auction do
   it 'exists' do
@@ -123,9 +124,63 @@ RSpec.describe Auction do
     it 'shows items by bidders' do
       expect(@auction.items_by_bidder(@attendee1)).to eq([@item1])
     end
+  end
 
-    it 'can close bidding' do
-      
+  context 'close auction' do
+    it 'has a date' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+      attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+      attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+      auction = Auction.new
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      expect(auction.date).to eq(Date.today.strftime('%d/%m/%Y'))
+
+      allow(auction).to receive(:date).and_return('11/08/2020')
+
+      expect(auction.date).to eq('11/08/2020')
+    end
+
+    it 'can close the auction' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+      attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+      attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+      auction = Auction.new
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+      item1.add_bid(attendee1, 22)
+      item1.add_bid(attendee2, 20)
+      item4.add_bid(attendee2, 30)
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15)
+      item5.add_bid(attendee1, 35)
+      auction.close_auction
+
+      expected =  {
+        item1 => attendee1,
+        item2 => 'Not Sold',
+        item3 => attendee2,
+        item4 => attendee3,
+        item5 => attendee1
+      }
+      expect(auction.close_auction).to eq(expected)
     end
   end
 end
