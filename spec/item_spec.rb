34b8ea61ddbf80d63ea66288c98d0b1a8ddf9aa1
Henry Schmid <henry.schmid1@gmail.com>
2021-08-11 10:21:54 -0600

    Add bidders test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -49,4 +49,14 @@ RSpec.describe Item do
 
     expect(item1.current_high_bidder).to eq([attendee1, 22])
   end
+
+  it 'it shows bidders' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expect(item1.bidders).to eq(["Bob", "Megan"])
+  end
 end
