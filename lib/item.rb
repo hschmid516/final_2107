a382938cea6f1f03e248a4eb734b6c314099e840
Henry Schmid <henry.schmid1@gmail.com>
2021-08-11 09:33:00 -0600

    Add methods for bids and highest bidder

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,22 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, bid)
+    @bids[attendee] = bid
+  end
+
+  def current_high_bid
+    current_high_bidder[1]
+  end
+
+  def current_high_bidder
+    @bids.max_by do |attendee, bid|
+      bid
+    end
   end
 end
