dfbaf63980364b29a65459e19a84f08f3e83d9f0
Henry Schmid <henry.schmid1@gmail.com>
2021-08-11 10:30:44 -0600

    Add close bidding test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -59,4 +59,20 @@ RSpec.describe Item do
 
     expect(item1.bidders).to eq(["Bob", "Megan"])
   end
+
+  it 'can close bidding' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+
+    item1.close_bidding
+    item1.add_bid(attendee3, 70)
+
+    expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+  end
 end
