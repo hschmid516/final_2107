2453d23823d4a163d73ed410f5b880303b931145
Henry Schmid <henry.schmid1@gmail.com>
2021-08-11 09:33:13 -0600

    Update test for budget

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe Attendee do
     attendee = Attendee.new(name: 'Megan', budget: '$50')
 
     expect(attendee.name).to eq('Megan')
-    expect(attendee.budget).to eq('$50')
+    expect(attendee.budget).to eq(50)
   end
 end
